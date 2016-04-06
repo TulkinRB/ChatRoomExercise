d1a1a9391eaf2977269c5d736800bfbea8f4b725
TulkinRB <tulkinrb@gmail.com>
2016-04-06 19:17:59 +0300

    Add some client code

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -1,5 +1,30 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
 /**
  * Created by User on 06/04/2016.
  */
 public class ChatClient {
+	
+	public static void main(String args[]) throws UnknownHostException, IOException{
+		Scanner in = new Scanner(System.in);
+		String host = getHost(in);
+		String nickname = getNickname(in);
+		Socket server = new Socket(host, 6655);
+		OutputStream serverOut = server.getOutputStream();
+		InputStream serverIn = server.getInputStream();
+		
+	}
+	
+	public static String getHost(Scanner in){
+		return "loopback"; //todo: user input
+	}
+	
+	public static String getNickname(Scanner in){
+		return "foobar"; //todo: user input
+	}
 }
