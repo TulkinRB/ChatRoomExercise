0d6de72cac44bd51b79467b6bb1d336c1873d6a5
Nitzan Bueno <nitzan.bueno@gmail.com>
2016-05-15 16:11:40 +0300

    Add handshake variables and response
    Add variables "HANDSHAKE_OK" and "HANDSHAKE_BAD" and responses
    Add info text for ChatClient

--- src/ChatClient.java
+++ src/ChatClient.java
@@ -11,36 +11,53 @@ import java.util.Scanner;
  * The main client-side program.
  */
 public class ChatClient {
-	
+
+
 	public static void main(String args[]) throws IOException, ParseException {
-		Scanner in = new Scanner(System.in);
-		String host = getHost(in);
-		String nickname = getNickname(in);
-		Socket server = new Socket(host, 6655);
-		PrintWriter serverOut = new PrintWriter(server.getOutputStream());
-		BufferedReader serverIn = new BufferedReader(new InputStreamReader(server.getInputStream()));
+		Scanner in = new Scanner(System.in); // User input
+		String host = getHost(in); // Hostname
+		String nickname = getNickname(in); // Nickname
+		Socket server = new Socket(host, 6655); // Server socket
+		PrintWriter serverOut = new PrintWriter(server.getOutputStream()); // Server output
+		BufferedReader serverIn = new BufferedReader(new InputStreamReader(server.getInputStream())); // Server input
 		handshake(serverIn, serverOut);
 	}
-	
+
+    /**
+     * Get hostname from user input.
+     * @param in The input scanner.
+     * @return The user input hostname.
+     */
 	public static String getHost(Scanner in){
 		return "127.0.0.1"; //todo: user input
 	}
-	
+
+    /**
+     * Get nickname from user input.
+     * @param in The input scanner.
+     * @return The user input nickname.
+     */
 	public static String getNickname(Scanner in){
 		System.out.print("Enter your nickname: ");
 		return in.nextLine();
 	}
-	
+
+    /**
+     * Perform a handshake between the client and the server.
+     * @param in The server input.
+     * @param out The server output.
+     * @return true if the handshake has succeeded, false if it has failed.
+     */
 	public static boolean handshake(BufferedReader in, PrintWriter out){
 		try {
-			Date serverDate = ChatServer.FORMAT.parse(in.readLine());
+			Date serverDate = ChatServer.HANDSHAKE_FORMAT.parse(in.readLine());
 			long oldTime = serverDate.getTime();
 			long newTime = oldTime - 1000*60*60*24;
 			Date newDate = new Date(newTime);
-			out.println(ChatServer.FORMAT.format(newDate));
+			out.println(ChatServer.HANDSHAKE_FORMAT.format(newDate));
+            return in.readLine().equals(ChatServer.HANDSHAKE_OK);
 		} catch (Exception e) {
 			return false;
 		}
-		return true;
 	}
 }
--- src/ChatServer.java
+++ src/ChatServer.java
@@ -19,7 +19,9 @@ public class ChatServer {
     private Timer timeOut;
     private HashSet<ClientData> clients;
 
-    public static final DateFormat FORMAT = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+    public static final DateFormat HANDSHAKE_FORMAT = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss");
+    public static final String HANDSHAKE_OK = "AFFIRMATIVE";
+    public static final String HANDSHAKE_BAD = "NEGATIVE";
 
     public ChatServer() {
         timeOut = new Timer();
@@ -38,11 +40,19 @@ public class ChatServer {
     public boolean handshake(BufferedReader in, PrintWriter out) throws IOException {
         try {
             Date now = new Date();
-            out.println(FORMAT.format(now));
-            Date newDate = FORMAT.parse(in.readLine());
-            return now.getTime() - newDate.getTime() == 1000*60*60*24;
+            out.println(HANDSHAKE_FORMAT.format(now));
+            Date newDate = HANDSHAKE_FORMAT.parse(in.readLine());
+            if(now.getTime() - newDate.getTime() == 1000*60*60*24) {
+                out.println(HANDSHAKE_OK);
+                return true;
+            }
+            else {
+                out.println(HANDSHAKE_BAD);
+                return false;
+            }
         }
         catch (ParseException e) {
+            out.println(HANDSHAKE_BAD);
             return false;
         }
     }
